edf59491951b3e49ab0ef4deab63f315111b911f
JavianTheGreek <javianbedward0917@gmail.com>
2022-12-31 15:50:22 -0500

    Updated Hero Component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -39,11 +39,14 @@ function Hero() {
                     {/* Heading for the right Container */}
                     <h2 className='text-4xl font-bold text-soft-orange'>New</h2>
                     
-                    <Container title="Hydrogen VS Electric Cars" description="Will hydrogen-fueled cars ever catch up to EVs?" />
-                    <Container title="The Downsides of AI Artisty" description="What are the possible adverse effects of on-demand AI image generation" />
-                    <div>
-                        <Container className="border border-none" title="IS VC Funding Drying Up?" description="Will hydrogen-fueled cars ever catch up to EVs?" />
+                    <div className='border-b-dark-grayish-blue border'>
+                        <Container title="Hydrogen VS Electric Cars" description="Will hydrogen-fueled cars ever catch up to EVs?" />
                     </div>
+                    <div className='border-b-dark-grayish-blue border'>
+                        <Container title="The Downsides of AI Artisty" description="What are the possible adverse effects of on-demand AI image generation" />
+                    </div>
+                    
+                    <Container className="border border-none" title="IS VC Funding Drying Up?" description="Will hydrogen-fueled cars ever catch up to EVs?" />
                     
                 </div>
             </div>
@@ -63,16 +66,16 @@ const HandleMobileImage = ( {isMobile} ) => (
 // 
 const HandleDesktopImage = ( {isDesktop} ) => (
     <div className="">
-        <img src={DesktopImage} alt="/" className='max-lg:hidden' />
+        <img src={DesktopImage} alt="/" className='max-md:hidden' />
     </div>
 );
 
 // 
 const Container = ({title, description}) => (
     <div className="md:mt-6">
-        <div className="border-b-dark-grayish-blue border">
+        <div className="">
             <div className='text-off-white my-2'>
-                <h3 className='font-bold my-4 text-2xl md:text-lg hover:text-soft-orange duration-300 ease-in-out cursor-pointer'>{title}</h3>
+                <h3 className='font-bold mt-6 mb-5 text-2xl md:text-lg hover:text-soft-orange duration-300 ease-in-out cursor-pointer'>{title}</h3>
                 <p className='text-thin text-lg md:text-xs text-grayish-blue pr-2 mb-6'>{description}</p>
             </div>
         </div>
